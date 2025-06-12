39cac4d71ccef52e835fb7bf6825168f61308c88
kms1601 <kms_1601@naver.com>
2025-06-13 00:43:40 +0900

    [update] change domains

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-export const BASE_URL = "https://www.1k6m0s1.com";
+export const BASE_URL = "https://www.1k6m0s1.xyz";
 export const SITE_NAME = "kms1601의 개발 블로그";
 export const DESCRIPTION = "저의 기록과 정보를 공유합니다.";
 export const BASE_PATH = "/src/posts";
